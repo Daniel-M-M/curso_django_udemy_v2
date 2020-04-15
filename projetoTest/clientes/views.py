0ef7b72fac9dd4ac81b8ff65dafe0cecd502268c
danielmariniello <acemarinielo@gmail.com>
2020-04-15 05:53:48 +0200

    CRUD read

--- projetoTest/clientes/views.py
+++ projetoTest/clientes/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Person
 
 
 def persons_list(request):
-    return render(request, 'pessoa.html')
+    persons = Person.objects.all()
+    return render(request, 'person.html', {'persons': persons})
